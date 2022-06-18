322fade8b2bc0a6baf94eae4a1c15da91f76e209
Lee Jaemin <jm1225@kakao.com>
2022-06-18 20:46:37 +0900

    feat: use function map of lists

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <button onClick={ articleSort } >정렬</button> */}
 
 
-      <List></List>
+      <Lists></Lists>
 
       <Modal title='제목' date={ '2022년 6월 18일' } content='상세 내용'></Modal>
 
@@ -44,81 +44,44 @@ const lists = [
   {
     id: 1,
     title: "남자 코트 추천",
-    like: 0,
-    today: Date()
+    likes: 0,
+    today: new Date()
   },
   {
     id: 2,
     title: "강남 우동 맛집",
-    like: 0,
-    today: Date()
+    likes: 0,
+    today: new Date()
   },
   {
     id: 3,
     title: "리액트 독학",
-    like: 0,
-    today: Date()
+    likes: 0,
+    today: new Date()
   }
 ]
 
-function List(){
-  const articles = [...lists]
-  const titles = articles.map(({ title }) => title)
-  const likes = articles.map(({ like }) => like)
-  const todays = articles.map(({ today }) => today)
-  console.log(titles)
-  let [likes0, setLikes0] = useState(likes[0]);
-  let [likes1, setLikes1] = useState(likes[1]);
-  let [likes2, setLikes2] = useState(likes[2]);
+function Lists(){
+  let articles = [...lists]
+  return articles.map((e)=>{
+    return (
+      <List key={ e.id } title={ e.title } likes={ e.likes } today={ e.today }></List>
+    )
+  })
+}
 
+function List(props){
+  let [title, setTitle] = useState(props.title);
+  let [likes, setLikes] = useState(props.likes);
+  let [today, setToday] = useState(props.today);
+  let todate = `${today.getFullYear()}년 ${today.getMonth()+1}월 ${today.getDate()}일`
   return (
-    <>
     <div className='list'>
-      <h3> { titles[0] } <span onClick={ ()=>{ setLikes0(likes0 + 1) } }> 👍 </span> { likes0 } </h3>
-      <p> { todays[0] } </p>
+      <h3> { title } <span onClick={ ()=>{ setLikes(likes + 1) } }> 👍 </span> { likes } </h3>
+      <p> { todate } </p>
       <hr/>
     </div>
-    <div className='list'>
-      <h3> { titles[1] } <span onClick={ ()=>{ setLikes1(likes1 + 1) } }> 👍 </span> { likes1 } </h3>
-      <p> { todays[1] } </p>
-      <hr/>
-    </div>
-    <div className='list'>
-      <h3> { titles[2] } <span onClick={ ()=>{ setLikes2(likes2 + 1) } }> 👍 </span> { likes2 } </h3>
-      <p> { todays[2] } </p>
-      <hr/>
-    </div>
-    </>
   )
-
-
-  // let [title, setArticle] = useState(titles);
-  // let [like, setLikes] = useState(likes);
-  // let [todate, setTodate] = useState(todays);
-  // console.log(todate)
-  // todate = setTodate(`${todate.getFullYear()}년 ${todate.getMonth()+1}월 ${todate.getDate()}일`)
-
-  // const article = articles.map((e)=>{
-  //   console.log(e)
-
-  //   return (
-  //     <div className='list' key={ e.id }>
-  //       <h3> { e.title } <span onClick={ ()=>{ setLikes(e.like + 1) } }> 👍 </span> { e.like } </h3>
-  //       <p> { e.todate } </p>
-  //       <hr/>
-  //     </div>
-  //   )
-  // })
-
-  // return article
-  
-  // return (
-  //   <div className='list'>
-  //     <h3> { article } <span onClick={ ()=>{ setGood(good + 1) } }> 👍 </span> { good } </h3>
-  //     <p> { todate } </p>
-  //     <hr/>
-  //   </div>
-  // )
 }
 
 function Modal(props){
